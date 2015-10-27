159abb0efff6415ef277a24e78b5c4b233870917
Swen <swenmeeu@gmail.com>
2015-10-27 13:33:52 +0100

    JsonSharp + Get methods added
    UNFINISHED SESSION

--- DataFlowWebservice/DataFlowWebservice/Controllers/ValuesController.cs
+++ DataFlowWebservice/DataFlowWebservice/Controllers/ValuesController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using DataFlowWebservice.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -10,15 +14,39 @@ namespace DataFlowWebservice.Controllers
     public class ValuesController : ApiController
     {
         // GET api/values
-        public IEnumerable<string> Get()
+        public IEnumerable<Position> Get()
         {
-            return new string[] { "value1", "value2" };
+            int amount = 100;
+            Position[] positions = new Position[amount];
+            for (int i = 0; i < amount; i++)
+            {
+                positions[i] = new Position().generate();
+            }
+            
+            return positions;
         }
 
         // GET api/values/5
-        public string Get(int id)
+        public Position Get(int unitId)
         {
-            return "value";
+            //string connectionString = "mongodb://root:root@145.24.222.160:27017/Dataflow";//ConfigurationManager.AppSettings["connectionString"];
+
+            //IMongoClient mongoClient = new MongoClient(connectionString);
+
+            //IMongoDatabase db = mongoClient.GetDatabase("test");
+            //var collection = db.GetCollection<BsonDocument>("positions");
+
+            //var positionObject = new Position(collection.ToJson().ToString());
+
+            var connectionString = "mongodb://145.24.222.160:27017";
+            var client = new MongoClient(connectionString);
+            var server = client.GetServer();
+            server.Ping();
+
+            Position position = new Position().generate();
+            position.unitId = unitId;
+
+            return position;
         }
 
         // POST api/values
