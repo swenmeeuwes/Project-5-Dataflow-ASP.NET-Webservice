92082dd268b24971ab7d07b88e3d8104a70e45c7
Swen <swenmeeu@gmail.com>
2015-12-08 11:54:32 +0100

    ADDED: Json ignore for MongoDB objectID
    ObjectID doesn't show up in the response results anymore

--- DataFlowWebservice/DataFlowWebservice/Models/Event.cs
+++ DataFlowWebservice/DataFlowWebservice/Models/Event.cs
@@ -9,6 +9,7 @@ namespace DataFlowWebservice.Models
 {
     public class Event
     {
+        [JsonIgnore]
         public ObjectId _id { get; set; }
         public DateTime dateTime { get; set; }
         public long unitId { get; set; }
