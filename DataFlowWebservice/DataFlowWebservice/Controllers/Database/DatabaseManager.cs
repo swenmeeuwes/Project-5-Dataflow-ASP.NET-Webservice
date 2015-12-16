f4c714c7125f06f636ccccffaf6f93c54ca5e1a7
swenmeeuwes <swenmeeu@gmail.com>
2015-12-16 19:27:56 +0100

    ADDED: Web.config strings

--- DataFlowWebservice/DataFlowWebservice/Controllers/Database/DatabaseManager.cs
+++ DataFlowWebservice/DataFlowWebservice/Controllers/Database/DatabaseManager.cs
@@ -1,6 +1,7 @@
 ﻿using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 
@@ -13,7 +14,7 @@ namespace DataFlowWebservice.Controllers.Database
         {
             // Bouwt een connection string
             MongoServerSettings settings = new MongoServerSettings();
-            settings.Server = new MongoServerAddress("145.24.222.160", 8010);
+            settings.Server = new MongoServerAddress(ConfigurationManager.AppSettings["DatabaseConnectionIpString"], Convert.ToInt32(ConfigurationManager.AppSettings["DatabaseConnectionPortString"]));
             // Maakt een MongoDB server object, met dit object kunnen we communiceren met de server. Het maakt kortom verbinding met de mongodb server
             MongoServer server = new MongoServer(settings);
             // Vraagt onze database op van de server, zodat we read/write kunnen toepassen
