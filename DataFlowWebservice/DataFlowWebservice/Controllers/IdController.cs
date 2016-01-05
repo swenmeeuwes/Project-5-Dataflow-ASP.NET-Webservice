9545a82d83ec0e3249489bb32c9eab1d0efe7c94
swenmeeuwes <swenmeeu@gmail.com>
2016-01-05 19:32:13 +0100

    ADDED: api/id
    Gets all the unique unit id's in position.csv

---
+++ DataFlowWebservice/DataFlowWebservice/Controllers/IdController.cs
@@ -0,0 +1,28 @@
+﻿using DataFlowWebservice.Controllers.Database;
+using DataFlowWebservice.Models;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DataFlowWebservice.Controllers
+{
+    public class IdController : ApiController
+    {
+        DatabaseManager databaseManager;
+        public IdController()
+        {
+            databaseManager = new DatabaseManager();
+        }
+        // GET: api/Id
+        public IEnumerable<long> Get()
+        {
+            IEnumerable<BsonValue> bsonValues = databaseManager.GetDatabase().GetCollection<Position>("positions").Distinct("unitId");
+            return BsonSerializer.Deserialize<List<long>>(bsonValues.ToJson());
+        }
+    }
+}
